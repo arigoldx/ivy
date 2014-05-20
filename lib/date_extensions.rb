80314ebf6d96c710634dfa78207901c373d6dbc2
Ari Gold <arigoldx@gmail.com>
2014-05-19 22:34:58 -0700

    more css, some layout, stubbed extension on Date, remembered to precompile assets

---
+++ lib/date_extensions.rb
@@ -0,0 +1,14 @@
+class Date
+  def self.foolpaljoe
+    puts "barkbarkbark"
+  end
+
+#  def flatten_end_date_select hash
+#    %w(1 2 3).map { |e| hash["end_date(#{e}i)"].to_i }
+#  end
+#
+#  def flatten_start_date_select hash
+#    %w(1 2 3).map { |e| hash["end_date(#{e}i)"].to_i }
+#  end
+
+end
